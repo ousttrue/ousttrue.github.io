ab24b83fd4e839a9bb52fed30dc99d579531fddf
ousttrue <ousttrue@gmail.com>
2023-03-13 03:58:39 +0900

    update

---
+++ src/components/layout.js
@@ -0,0 +1,23 @@
+import React from "react"
+// import Paper from '@material-ui/core/Paper'
+
+const Header = () => (
+    <header>
+        SITE TITLE
+    </header>
+)
+const Footer = () => (
+    <footer>
+        Copyright
+    </footer>
+)
+const Layout = ({ children }) => (
+    <div>
+        <Header/>
+
+        {children}
+
+        <Footer/>
+    </div>
+)
+export default Layout
--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -1,14 +1,15 @@
 import React from "react"
 import { MDXRenderer } from "gatsby-plugin-mdx"
+import Layout from "../components/layout.js"
 
 export default function PostTempalte ( { pageContext }){
     const { node } = pageContext
     return (
-        <div>
+        <Layout>
             <h2>{node.frontmatter.title}</h2>
             <MDXRenderer>
                 {node.body}
             </MDXRenderer>
-        </div>
+        </Layout>
     )
 }
