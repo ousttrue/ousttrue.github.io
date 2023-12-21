4b49794eb607d2f1984a194faf62ca09815d4d66
ousttrue <ousttrue@gmail.com>
2023-12-22 02:47:29 +0900

    all posts

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,6 +49,7 @@ const config = {
           path: 'posts',
           routeBasePath: 'posts',
           showReadingTime: false,
+          blogSidebarCount: 'ALL',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
