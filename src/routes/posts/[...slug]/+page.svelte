a98562294fb72efb6b18eb5cae45f73922713fa8
ousttrue <ousttrue@gmail.com>
2023-12-31 13:33:10 +0900

    remark/rehype TOC

--- src/routes/posts/[...slug]/+page.svelte
+++ src/routes/posts/[...slug]/+page.svelte
@@ -1,22 +1,39 @@
-<script>
-  import Markdown from "svelte-exmarkdown";
-  import { gfmPlugin } from "svelte-exmarkdown/gfm";
+<script lang="ts">
+  import type { PostType } from "$lib/getPosts";
+  export let data: PostType;
+
+  import { unified } from "unified";
+  import remarkParse from "remark-parse";
+  import remarkSlug from "remark-slug";
+  import remarkToc from "remark-toc";
+  import remark2rehype from "remark-rehype";
+  import rehypeSlug from "rehype-slug";
+  import rehypeToc from "rehype-toc";
   import rehypeHighlight from "rehype-highlight";
+  import rehypeStringify from "rehype-stringify";
   import "highlight.js/styles/github.css";
   import "./markdown.css";
   import PostTitle from "../PostTitle.svelte";
 
-  const plugins = [
-    gfmPlugin(),
-    {
-      rehypePlugin: [rehypeHighlight, { ignoreMissing: true }],
-    },
-  ];
-
-  export let data;
+  const md2html = unified()
+    // mdast
+    .use(remarkParse)
+    // .use(remarkSlug)
+    // .use(remarkToc, {
+    //   heading: "目次",
+    //   tight: true,
+    // })
+    // hast
+    .use(remark2rehype)
+    .use(rehypeSlug)
+    .use(rehypeToc)
+    .use(rehypeHighlight)
+    .use(rehypeStringify);
+  const file = md2html.processSync(data.body);
+  // console.log(file);
 </script>
 
 <PostTitle post={data} />
 <div class="markdown">
-  <Markdown md={data.body} {plugins} />
+  {@html file.value}
 </div>
