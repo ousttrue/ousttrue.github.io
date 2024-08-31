9b6d904f3032932ebfc0fa73460b2f7b1d45d17b
ousttrue <ousttrue@gmail.com>
2024-08-31 21:38:03 +0900

    vite vanilla

--- src/consts.ts
+++
@@ -1,6 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = '三次元日誌';
-export const SITE_DESCRIPTION = 'powered by astro';
-export const GITHUB_URL = 'https://github.com/ousttrue/ousttrue.github.io';
