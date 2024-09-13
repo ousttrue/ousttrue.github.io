bef25825d759c18bea6bed5325e6b25ec09e29b8
ousttrue <ousttrue@gmail.com>
2024-09-14 02:36:10 +0900

    github

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,7 +1,7 @@
 export default function Layout(props) {
   return (<html lang="en">
     <head>
-      <meta charset="UTF-8" />
+      <meta charSet="UTF-8" />
       <link rel="icon" type="image/svg+xml" href="/vite.svg" />
       <title>{props.title ? props.title : "三次元日誌"}</title>
     </head>
@@ -12,6 +12,7 @@ export default function Layout(props) {
           <a href="/">三次元日誌(vite)</a>
           <a href="/posts/">posts</a>
           <a href="/tags/">tags</a>
+          <a href="https://github.com/ousttrue/ousttrue.github.io">github</a>
         </nav>
       </header>
       {props.children}
